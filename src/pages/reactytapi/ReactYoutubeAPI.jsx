2462a99afa3afa9e109354f5e1be309f3a8634d9
Samit Koyom <samitkoyom@gmail.com>
2021-04-02 23:10:05 +0700

    update youtube api

--- src/pages/reactytapi/ReactYoutubeAPI.jsx
+++ src/pages/reactytapi/ReactYoutubeAPI.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 /* eslint-disable jsx-a11y/alt-text */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTable } from "@fortawesome/free-solid-svg-icons"
@@ -7,7 +8,7 @@ const ReactYoutubeAPI = () => {
     return (
         <>
             <h1 className="text-3xl text-black pb-6">React Youtube API</h1>
-            <p>Content here...</p>
+            <iframe width="100%" height="720" src="https://www.powtoon.com/embed/dNdKm4Q2ji1/" frameborder="0" allowfullscreen></iframe>
         </>
     )
 }
